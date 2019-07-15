150267b116bc519cd53561449aab4caa9d3d7f76
이오주 <badger@administratorui-MacBookPro-4.local>
2019-07-15 14:58:12 +0900

    FE init setting

--- FE/src/components/App.js
+++ FE/src/components/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+  return <div className="App">hello</div>;
 }
 
 export default App;
