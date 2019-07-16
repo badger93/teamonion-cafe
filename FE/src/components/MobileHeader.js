dd4c3ebf805a3c4f2ec581f2829f59fea040b216
hyunjae <steveysung91@gmail.com>
2019-07-16 16:29:54 +0900

    pcheader

---
+++ FE/src/components/MobileHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MobileHeader = () => {
+  return <div>mobileheader</div>;
+};
+
+export default MobileHeader;
---
+++ FE/src/components/PcHeader.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import tmonglogo from '../image/tmonglogo.png';
+import '../styles/PcHeader.scss';
+
+const PcHeader = () => {
+  const dummyUser = { id: 'hyunjae' };
+  const isLogined = false;
+  return (
+    <div className="header_pc">
+      <div className="header_pc-wrap">
+        <div className="header_pc-logo">
+          <img src={tmonglogo} alt="logo" />
+          <Link to="/" />
+        </div>
+        <div className="header_pc-column">
+          <div className="header_pc-column-top">
+            <span className="header_pc-welcome">{`반갑습니다 ${
+              dummyUser.id
+            }님`}</span>
+            <span className="divider">|</span>
+            {isLogined ? (
+              <Link to="/logout">LogOut</Link>
+            ) : (
+              <>
+                <Link to="/signin">Log In</Link>
+                <span className="divider">|</span>
+                <Link to="/signup">Sign Up</Link>
+              </>
+            )}
+          </div>
+          <div className="header_pc-column-bottom">
+            <Link to="/">Menu</Link>
+
+            <Link to="/myorder">MyOrder</Link>
+
+            <Link to="/user-info">MyPage</Link>
+
+            <Link to="/cart">Cart</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PcHeader;
