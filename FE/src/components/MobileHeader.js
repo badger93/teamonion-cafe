906e46279f0a38ea0369a9fada15595c24555438
hyunjae <steveysung91@gmail.com>
2019-07-16 17:49:14 +0900

    mobileheader admin

--- FE/src/components/MobileHeader.js
+++ FE/src/components/MobileHeader.js
@@ -13,18 +13,29 @@ const MobileHeader = () => {
     <div className="header_mobile-wrap">
       <div className="header_mobile-logo">
         <img src={tmonglogo} alt="logo" />
-        <Link to="/" />
+        {isAdmin ? <Link to="/admin/order-manage" /> : <Link to="/" />}
       </div>
       <div className="header_mobile-column">
-        <Link to="/user-info">
-          <FontAwesomeIcon icon={faUser} />
-        </Link>
-        <Link to="/myorder">
-          <FontAwesomeIcon icon={faCoffee} />
-        </Link>
-        <button className="mobile-list">
-          <FontAwesomeIcon icon={faBars} />
-        </button>
+        {isAdmin ? (
+          <>
+            <Link to="/admin/order-manage">주문현황</Link>
+            <Link to="/admin/menu-manage">메뉴관리</Link>
+            <Link to="/admin/order-history">주문히스토리</Link>
+            <Link to="/admin/member-manage">사용자관리</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/user-info">
+              <FontAwesomeIcon icon={faUser} size="2x" />
+            </Link>
+            <Link to="/myorder">
+              <FontAwesomeIcon icon={faCoffee} size="2x" />
+            </Link>
+            <button className="mobile-list">
+              <FontAwesomeIcon icon={faBars} size="2x" />
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
