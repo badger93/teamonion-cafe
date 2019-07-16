d5f3d11ac3b3cf14b8abd67ff95cd7f229404d57
hyunjae <steveysung91@gmail.com>
2019-07-16 17:37:36 +0900

    mobileheader user

--- FE/src/components/MobileHeader.js
+++ FE/src/components/MobileHeader.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import '../styles/MobileHeader.scss';
+import { Link } from 'react-router-dom';
+import tmonglogo from '../image/tmonglogo.png';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faCoffee, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const MobileHeader = () => {
-  return <div>mobileheader</div>;
+  const isAdmin = true;
+
+  return (
+    <div className="header_mobile-wrap">
+      <div className="header_mobile-logo">
+        <img src={tmonglogo} alt="logo" />
+        <Link to="/" />
+      </div>
+      <div className="header_mobile-column">
+        <Link to="/user-info">
+          <FontAwesomeIcon icon={faUser} />
+        </Link>
+        <Link to="/myorder">
+          <FontAwesomeIcon icon={faCoffee} />
+        </Link>
+        <button className="mobile-list">
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MobileHeader;
