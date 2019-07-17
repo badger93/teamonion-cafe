11547e4f1490ab6e2e510128dc9c8453d3440e0e
hyunjae <steveysung91@gmail.com>
2019-07-17 15:50:31 +0900

    myhistory

--- FE/src/routes/user/UserInfo/UserInfoPresenter.js
+++ FE/src/routes/user/UserInfo/UserInfoPresenter.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import UserInfoCard from '../../../components/UserInfoCard';
+
 import '../../../styles/UserInfoPresenter.scss';
-const UserInfoPresenter = ({ id, point }) => {
+import MyHistory from '../../../components/MyHistory';
+
+const UserInfoPresenter = ({ id, point, columns, rows }) => {
   return (
     <div className="userinfo_section">
       <div className="userinfo_wrapper">
         <div className="userinfo_title_myinfo">내 정보</div>
         <UserInfoCard id={id} point={point} />
         <div className="userinfo_title_history">주문 히스토리</div>
+        <div className="myhistory-container">
+          <MyHistory columns={columns} rows={rows} />
+        </div>
       </div>
     </div>
   );
@@ -17,6 +23,8 @@ const UserInfoPresenter = ({ id, point }) => {
 UserInfoPresenter.propTypes = {
   id: propTypes.string.isRequired,
   point: propTypes.number.isRequired,
+  columns: propTypes.array.isRequired,
+  rows: propTypes.array.isRequired,
 };
 
 export default UserInfoPresenter;
