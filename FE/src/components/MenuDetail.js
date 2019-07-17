01bac5b7733842f8594b6dbfbee90a25ccc26e93
이오주 <badger@tmon.co.kr>
2019-07-17 17:12:38 +0900

    menupopup 스타일링

--- FE/src/components/MenuDetail.js
+++ FE/src/components/MenuDetail.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import '../styles/MenuDetail.scss';
 
 const MenuDetail = (props) => {
   const {
-    menuDetailData: {},
+    menuDetailData: {
+ name, price, information, imagePath 
+},
   } = props;
+
+  const popupClose = () => {
+    document.querySelector('.menuDetail').style.display = 'none';
+  };
 
   return (
     <>
-      <div>{JSON.stringify(menuDetailData)}</div>
       <div className="menuDetail">
         <aside>
           <div className="img-area">
-            <img src="/" />
+            <img src={imagePath} alt="제품이미지" />
           </div>
         </aside>
         <main className="main">
-          <div className="title" />
-          <div className="price" />
-          <div className="information" />
-          <div className="btnArea" />
+          <input
+            className="closeBtn"
+            type="button"
+            value="X"
+            onClick={() => popupClose()}
+          />
+          <div className="titleArea">
+            <div className="title">{name}</div>
+          </div>
+          <div className="informationArea">
+            <div className="price">{`₩ ${price}`}</div>
+            <div className="information">{information}</div>
+          </div>
+          <div className="btnArea">
+            <input className="buyBtn" type="button" value="구매" />
+            <input className="cartBtn" type="button" value="장바구니" />
+          </div>
         </main>
       </div>
     </>
--- FE/src/components/MenuListItem.js
+++ FE/src/components/MenuListItem.js
@@ -10,11 +10,24 @@ const MenuListItem = (props) => {
   } = props;
   const { item, mapDetailData } = props;
 
+  const popup = document.querySelector('.menuDetail');
+  const popupWidth = popup.style.width;
+  const popupHeight = popup.style.height;
+  const popupLeft = (window.innerWidth - popupWidth) / 2;
+  const popupTop = (window.innerHeight - popupHeight) / 2;
+
+  const showPopup = (left, top) => {
+    popup.style.display = 'block';
+    popup.style.left = left;
+    popup.style.top = top;
+  };
+
   return (
     <div
       className="menuListItem"
       onClick={() => {
         mapDetailData(item);
+        showPopup(popupLeft, popupTop);
       }}
     >
       <div className="img-area">
