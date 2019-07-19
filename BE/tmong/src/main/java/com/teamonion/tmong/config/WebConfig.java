43360e6fd109da68cc7862b83cacf6d27a88135a
이창환 <nowmoon77@tmon.co.kr>
2019-07-19 15:24:47 +0900

    chore

--- BE/tmong/src/main/java/com/teamonion/tmong/config/WebConfig.java
+++ BE/tmong/src/main/java/com/teamonion/tmong/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor());
+        //registry.addInterceptor(jwtInterceptor());
     }
 
     @Bean
