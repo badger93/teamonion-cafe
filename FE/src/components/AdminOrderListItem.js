f04d637c9c0bf59b2238f19aa1e9201cadb4edfa
이오주 <badger@tmon.co.kr>
2019-07-19 17:36:43 +0900

    어드민 add pops 받아오기

---
+++ FE/src/components/AdminOrderListItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AdminOrderListItem = ({
+  order_id, menus, paymentType, paid, made, pickup, createdDate, amount, Member_id,
+}) => {
+  const aa = '';
+  return (
+    <>
+      <div>{amount}</div>
+    </>
+  );
+};
+
+export default AdminOrderListItem;
--- FE/src/routes/admin/AdminOrderManage/AdminOrderManagePresenter.js
+++ FE/src/routes/admin/AdminOrderManage/AdminOrderManagePresenter.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import AdminMakingArea from '../../../components/AdminMakingArea';
 
 const AdminOrderManagePresenter = ({
   currentOrderList,
   setCurrentOrderList,
 }) => {
-  console.dir(currentOrderList);
+  const beforeList = currentOrderList.filter(item => item.made === false);
+  const afterList = currentOrderList.filter(item => item.made === true);
+
   return (
     <>
       <h1>주문현황</h1>
       <div className="orderContainer">
-        <div className="premaking-area">
-          <h2>제작중</h2>
-          <div className="orderBoard">
-            <div className="orderLists">
-              <div className="orderListItem" />
-            </div>
-          </div>
+        <AdminMakingArea list={beforeList} areaName="before" />
+        <div className="arrowContainer">
+          <img src="" alt="화살표이미지" />
         </div>
-        <div className="premaking-area">
-          <h2>제작완료</h2>
-          <div className="orderBoard">
-            <div className="orderLists">
-              <div className="orderListItem" />
-            </div>
-          </div>
-        </div>
+        <AdminMakingArea list={afterList} areaName="after" />
       </div>
     </>
   );
