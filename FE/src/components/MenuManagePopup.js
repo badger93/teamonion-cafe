8276388fb2b13373c8ec14c9acfe827a63a468d6
이오주 <badger@tmon.co.kr>
2019-07-23 16:13:05 +0900

    react data grid 적용, 메뉴 이미지 미리보기 버그 수정

--- FE/src/components/MenuManagePopup.js
+++ FE/src/components/MenuManagePopup.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect, useRef } from 'react';
 import propTypes from 'prop-types';
 import '../styles/MenuManagePopup.scss';
 import inputImgPreview from '../utils/inputImgPreview';
+import { closePopup } from '../utils/popup';
 
-const MenuManagePopup = ({ menuPopupData, updateItem, createItem }) => {
+const MenuManagePopup = ({
+  menuPopupData, updateItem, createItem, popupRef,
+}) => {
   const {
     id, name, price, information, imageFile,
   } = menuPopupData;
@@ -12,55 +15,79 @@ const MenuManagePopup = ({ menuPopupData, updateItem, createItem }) => {
   const [popupInformation, setPopupInformation] = useState('');
   const [popupFile, setPopupFile] = useState('');
   const isEdit = Object.keys(menuPopupData).length > 1;
-  const inputImgRef = useRef(null); // 인풋 미리보기 이미지 태그
-  const fileInputRef = useRef(null); // 인풋 파일 태그
+  const inputImgRef = useRef(null); // 인풋 미리보기 img 태그
+  const fileInputRef = useRef(null); // 파일 input 태그
 
   useEffect(() => {
-    if (isEdit) {
+    if (isEdit) { // 수정모드
       setPopupName(name);
       setPopupPrice(price);
       setPopupInformation(information);
       setPopupFile(imageFile);
-    } else {
+      inputImgRef.current.setAttribute('src', '');
+    } else { // 추가모드
       setPopupName('');
       setPopupPrice('');
       setPopupInformation('');
       setPopupFile('');
+      inputImgRef.current.setAttribute('src', ''); // 이미지 미리보기
     }
   }, [menuPopupData]);
 
   return ( // name, price, information, imageFile(src)
     <div className="MenuManagePopup">
+      <input type="button" value="x" className="closeBtn" onClick={(e) => { closePopup(e, popupRef.current); }} />
+      <h1>{(isEdit) ? '수정' : '추가'}</h1>
       <form
         encType="multipart/form-data"
         className="MenuManageForm"
-        onSubmit={(e) => {
+        onSubmit={(e) => { // 폼 서브밋
           e.preventDefault();
-          const payload = {
-            name: popupName, price: popupPrice, information: popupInformation, imageFile: popupFile,
-          };
-          if (isEdit) {
-            updateItem(id, payload);
+          if (popupName && popupPrice && popupInformation && popupFile) {
+            const payload = {
+              name: popupName, price: popupPrice, information: popupInformation, imageFile: popupFile,
+            };
+            if (isEdit) {
+              updateItem(id, payload);
+              closePopup(e, popupRef.current);
+            } else {
+              createItem(payload, popupRef);
+              closePopup(e, popupRef.current);
+            }
           } else {
-            createItem(payload);
+            alert('내용을 마저 채워주세요');
           }
         }}
       >
-        <input type="text" value={popupName} onChange={e => setPopupName(e.target.value)} className="nameInput" />
-        <input type="text" value={popupPrice} onChange={e => setPopupPrice(e.target.value)} className="price" />
-        <input type="text" value={popupInformation} onChange={e => setPopupInformation(e.target.value)} className="information" />
-        <img src="" alt="" ref={inputImgRef} />
-        <input
-          type="file"
-          value={popupFile}
-          className="fileInput"
-          ref={fileInputRef}
-          onChange={(e) => {
-            inputImgPreview(fileInputRef.current, inputImgRef.current);
-            setPopupFile(e.target.value);
-          }}
-        />
-        <input type="submit" className="submitPopup" />
+        <div className="nameArea inputArea">
+          <div className="areaTitle">상품명</div>
+          <input type="text" value={popupName} onChange={e => setPopupName(e.target.value)} className="nameInput" />
+        </div>
+        <div className="priceArea inputArea">
+          <div className="areaTitle">가격</div>
+          <input type="text" value={popupPrice} onChange={e => setPopupPrice(e.target.value)} className="priceInput" />
+        </div>
+        <div className="informationArea inputArea">
+          <div className="areaTitle">설명</div>
+          <textarea value={popupInformation} onChange={e => setPopupInformation(e.target.value)} className="informationInput" />
+        </div>
+        <div className="imgInputArea inputArea">
+          <div className="areaTitle">이미지</div>
+          <input
+            type="file"
+            value={popupFile}
+            className="fileInput"
+            ref={fileInputRef}
+            onChange={(e) => {
+              inputImgPreview(fileInputRef.current, inputImgRef.current);
+              setPopupFile(e.target.value);
+            }}
+          />
+          <div className="previewImgWrap">
+            <img src="" alt="" ref={inputImgRef} className="imgInput" />
+          </div>
+          <input type="submit" value="저  장" className="submitPopup" />
+        </div>
       </form>
     </div>
   );
@@ -70,12 +97,14 @@ MenuManagePopup.defaultProps = {
   menuPopupData: {},
   updateItem: () => {},
   createItem: () => {},
+  popupRef: {},
 };
 
 MenuManagePopup.propTypes = {
-  menuPopupData: propTypes.objectOf(propTypes.string),
+  menuPopupData: propTypes.objectOf(propTypes.oneOfType([propTypes.string, propTypes.number])),
   updateItem: propTypes.func,
   createItem: propTypes.func,
+  popupRef: propTypes.objectOf(propTypes.Element),
 };
 
 export default MenuManagePopup;
--- FE/src/utils/inputImgPreview.js
+++ FE/src/utils/inputImgPreview.js
@@ -1,14 +1,17 @@
 const inputImgPreview = (inputDom, imgDom) => {
   const file = inputDom;
   const reader = new FileReader();
-  reader.readAsDataURL(file.files[0]);
-  reader.onload = () => {
-    console.log(reader.result);
-    imgDom.setAttribute('src', reader.result);
-  };
-  reader.onerror = (error) => {
-    console.log('Error: ', error);
-  };
+  if (file.files[0]) {
+    reader.readAsDataURL(file.files[0]);
+    reader.onload = () => {
+      imgDom.setAttribute('src', reader.result);
+    };
+    reader.onerror = (error) => {
+      console.log('Error: ', error);
+    };
+  } else {
+    imgDom.setAttribute('src', '');
+  }
 };
 
 
