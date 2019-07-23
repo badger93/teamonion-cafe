1ae5d3a49cb7af31a6c1ad13acc31db87946b473
이오주 <badger@tmon.co.kr>
2019-07-23 16:37:00 +0900

    이미지 미리보기 수정시 안보이는 문제 해결

--- FE/src/utils/inputImgPreview.js
+++ FE/src/utils/inputImgPreview.js
@@ -1,7 +1,7 @@
 const inputImgPreview = (inputDom, imgDom) => {
   const file = inputDom;
   const reader = new FileReader();
-  if (file.files[0]) {
+  if (file.value) {
     reader.readAsDataURL(file.files[0]);
     reader.onload = () => {
       imgDom.setAttribute('src', reader.result);
