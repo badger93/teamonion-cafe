3e520b335940d645e8e9bc8b489d04ffa40dd48f
이창환 <nowmoon77@tmon.co.kr>
2019-07-25 11:56:05 +0900

    Refactor : global exception, data.sql

--- BE/tmong/src/main/java/com/teamonion/tmong/exception/GlobalExceptionHandler.java
+++ BE/tmong/src/main/java/com/teamonion/tmong/exception/GlobalExceptionHandler.java
@@ -3,9 +3,8 @@ package com.teamonion.tmong.exception;
 import com.teamonion.tmong.validate.ValidationExceptionControllerAdvice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -13,10 +12,9 @@ public class GlobalExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(ValidationExceptionControllerAdvice.class);
 
     @ExceptionHandler(HandleRuntimeException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public GlobalExceptionResponse handleCustomException(HandleRuntimeException e) {
-        log.debug("GlobalExceptionResponse : {}", e.getErrorMessage());
-        return new GlobalExceptionResponse(e.getErrorMessage());
+    public ResponseEntity<String> handleCustomException(HandleRuntimeException e) {
+        log.debug("global exception : {}", e.getErrorMessage());
+        return new ResponseEntity<>(e.getErrorMessage(), e.getHttpStatus());
     }
 
 }
--- BE/tmong/src/main/resources/data.sql
+++ BE/tmong/src/main/resources/data.sql
@@ -1,13 +1,14 @@
-INSERT INTO MEMBER (id, member_id, member_role, password, point) VALUES (1, 'onion', 'ADMIN', '123456789a', '10000');
-INSERT INTO MEMBER (id, member_id, member_role, password, point) VALUES (2, 'onion2', 'NORMAL', '123456789a', '3000');
+INSERT INTO MEMBER (id, member_id, member_role, password, point) VALUES (1, 'onion', 'ADMIN', '123456789a', 10000);
+INSERT INTO MEMBER (id, member_id, member_role, password, point) VALUES (2, 'onion2', 'NORMAL', '123456789a', 3000);
+INSERT INTO MEMBER (id, member_id, member_role, password, point) VALUES (3, 'onion3', 'NORMAL', '123456789a', 99999999);
 
-INSERT INTO MENU (id, image_path, information, name, price) VALUES (1, 'image_path_test', 'information_test', 'americano', '1000');
-INSERT INTO MENU (id, image_path, information, name, price) VALUES (2, 'image_path_test', 'information_test', 'choco', '1500');
-INSERT INTO MENU (id, image_path, information, name, price) VALUES (3, 'image_path_test', 'information_test', 'measiltee', '1000');
-INSERT INTO MENU (id, image_path, information, name, price) VALUES (4, 'image_path_test', 'information_test', 'orangejuice', '1000');
+INSERT INTO MENU (id, image_path, information, name, price) VALUES (1, 'image_path_test', 'information_test', 'americano', 1000);
+INSERT INTO MENU (id, image_path, information, name, price) VALUES (2, 'image_path_test', 'information_test', 'choco', 1500);
+INSERT INTO MENU (id, image_path, information, name, price) VALUES (3, 'image_path_test', 'information_test', 'measiltee', 1000);
+INSERT INTO MENU (id, image_path, information, name, price) VALUES (4, 'image_path_test', 'information_test', 'orangejuice', 1000);
 
-INSERT INTO ORDERS (id, amount, buyer_id, created_date, made, paid, payment_type, pickup) VALUES (1, '2500', 1, '2019-07-24T16:39:31.434', false, true, 'POINT', false);
-INSERT INTO ORDERS (id, amount, buyer_id, created_date, made, paid, payment_type, pickup) VALUES (2, '2000', 2, '2019-07-24T17:48:22.123', false, false, 'CASH', false);
+INSERT INTO ORDERS (id, amount, buyer_id, created_date, made, paid, payment_type, pickup) VALUES (1, 2500, 1, '2019-07-24T16:39:31.434', false, true, 'POINT', false);
+INSERT INTO ORDERS (id, amount, buyer_id, created_date, made, paid, payment_type, pickup) VALUES (2, 2000, 2, '2019-07-24T17:48:22.123', false, false, 'CASH', false);
 
 INSERT INTO ORDERS_MENU_LIST (orders_id, menu_list_id) VALUES (1, 1);
 INSERT INTO ORDERS_MENU_LIST (orders_id, menu_list_id) VALUES (1, 2);
