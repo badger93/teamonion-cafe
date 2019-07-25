c6a0ae18f62056f08cb3410f3ee27a228ae7f693
hyunjae <steveysung91@gmail.com>
2019-07-25 10:29:43 +0900

    update

--- FE/src/api/userApi.js
+++ FE/src/api/userApi.js
@@ -3,15 +3,21 @@ import fetchClient from './axios';
 
 const axios = fetchClient();
 
-export const duplicateCheckApi = data => axios.get('api/members/overlap', { params: { memberId: data } });
+export const duplicateCheckApi = data => axios.post('api/members/overlap', data);
 
 export const signUpApi = data => axios.post('api/members', data);
 
 export const signInApi = data => axios.post('api/members/login', data);
 
 
-export const userOrderAPI = (memberId, pickup = true, size = 20, page = 1) => axios.get(`api/${memberId}/orders`, { params: { pickup, page, size } });
+export const userOrderHistoryAPI = memberId => axios.get(`api/${memberId}/orders`);
 
-// export const myOrderAPI = memberId => axios.get(`/api/${memberId}/orders/state`);
+export const myOrderAPI = memberId => axios.get(`/api/${memberId}/orders/state`);
 
-export const myPointApi = memberId => axios.get(`/api/members/${memberId}/point`);
+// admin
+// '/api/members?page=${page}&size=${itemSize}'
+export const getUserList = ({ itemSize, page }) => axios.get('https://my-json-server.typicode.com/badger012/mockserver/members');
+
+export const setUserPoint = ({ id, changePoint }) => axios.put(`/api/members/${id}/point`, { id, changePoint });
+
+export const searchUser = memberId => axios.get(`/api/members/${memberId}`);
