f1d2ae10105855d68be84676d469a9a72b33076c
이오주 <badger@tmon.co.kr>
2019-07-30 12:46:46 +0900

    FE 빌드 BE resources로

---
+++ BE/tmong/src/main/resources/precache-manifest.4743766055a55f37dda6883a522a8b4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5564cc4178ee0df12984b703618bbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc02a58de3ee1d19ae5a",
+    "url": "/static/css/main.105dd023.chunk.css"
+  },
+  {
+    "revision": "2b32e3aef7a7790739ca",
+    "url": "/static/js/2.83acfddb.chunk.js"
+  },
+  {
+    "revision": "bc02a58de3ee1d19ae5a",
+    "url": "/static/js/main.959405f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1de76b9ff0d1da05ad41ee5f907df38e",
+    "url": "/static/media/tmonglogo.1de76b9f.png"
+  }
+]);
