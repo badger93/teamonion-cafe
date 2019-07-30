a9f4047e8bfa20838909f2cee40b2992a439b911
김다희 <sefide36@tmon.co.kr>
2019-07-30 14:18:57 +0900

    Refactor : menuImagePath

--- BE/tmong/src/main/java/com/teamonion/tmong/config/WebConfig.java
+++ BE/tmong/src/main/java/com/teamonion/tmong/config/WebConfig.java
@@ -13,8 +13,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${download-path}")
-    private String downloadPath;
+    @Value("${download-path-file}")
+    private String downloadPathFile;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -37,7 +37,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("file:/" + downloadPath);
+                .addResourceLocations(downloadPathFile);
     }
 
 }
