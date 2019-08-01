cc350431dc1854ed979a94ab56e7a9273e810f43
김다희 <sefide36@tmon.co.kr>
2019-08-01 12:17:38 +0900

    Add : Check MessagHeader Info

--- BE/tmong/src/main/java/com/teamonion/tmong/order/WebSocketController.java
+++ BE/tmong/src/main/java/com/teamonion/tmong/order/WebSocketController.java
@@ -8,7 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 public class WebSocketController {
@@ -18,13 +22,38 @@ public class WebSocketController {
     @Autowired
     private OrdersService ordersService;
 
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     @MessageMapping("/api/orders/update")
     @SendTo("/topic/order")
-    public OrdersResponse updateOrder(@Payload OrdersUpdateRequest ordersUpdateRequest) {
+    public OrdersResponse updateOrder(@Payload OrdersUpdateRequest ordersUpdateRequest
+            , SimpMessageHeaderAccessor simpMessageHeaderAccessor) {
+
+        Principal user = simpMessageHeaderAccessor.getUser();
+
+
+        // 상태바꾼 관리자
+        // simpMessagingTemplate.convertAndSendToUser(user.getName(),"/topic/order",ordersUpdateRequest);
+
+        // 상태의 주인공
+
         log.info("--------------------------------");
         log.info("-------------updateOrder--------");
         log.info("payload ordersupdaterequest : {}", ordersUpdateRequest);
         log.info("--------------------------------");
+        log.info("--------------------------------");
+        log.info("User Name : {}" , user.getName());
+        log.info("User Info : {}" , user.toString());
+        log.info("--------------------------------");
+        log.info("Session Id : {}" , simpMessageHeaderAccessor.getSessionId());
+        log.info("Subscription Id : {}" , simpMessageHeaderAccessor.getSubscriptionId());
+        log.info("Destination : {}" , simpMessageHeaderAccessor.getDestination());
+        log.info("Message Type : {}" , simpMessageHeaderAccessor.getMessageType());
+
+        log.info("--------------------------------");
+        log.info("DetailLogMessage : {}" , simpMessageHeaderAccessor.getDetailedLogMessage(ordersUpdateRequest));
+
         return ordersService.updateOrder(ordersUpdateRequest);
     }
 }
