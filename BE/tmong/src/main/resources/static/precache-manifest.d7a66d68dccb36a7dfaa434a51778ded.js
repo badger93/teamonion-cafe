0ee341fadef64f39609738d0b1f9d4a9b6df732e
이창환 <nowmoon77@tmon.co.kr>
2019-08-01 12:18:50 +0900

    chore

---
+++ BE/tmong/src/main/resources/static/precache-manifest.d7a66d68dccb36a7dfaa434a51778ded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0446520d1c68e05bf60d33eaa2e18ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff5d56a728fb8f36601a",
+    "url": "/static/css/main.978aa3af.chunk.css"
+  },
+  {
+    "revision": "13381d72d36c284944fa",
+    "url": "/static/js/2.51b7d156.chunk.js"
+  },
+  {
+    "revision": "ff5d56a728fb8f36601a",
+    "url": "/static/js/main.4f3e7323.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1de76b9ff0d1da05ad41ee5f907df38e",
+    "url": "/static/media/tmonglogo.1de76b9f.png"
+  }
+]);
