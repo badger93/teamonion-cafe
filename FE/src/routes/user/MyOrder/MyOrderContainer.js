4d4eb3bb264bb27e400a2ef8c9cd056917f0a310
hyunjae <steveysung91@gmail.com>
2019-08-01 10:34:03 +0900

    Auto stash before merge of "websocket-test-branch-local" and "origin/websocket-test-branch"

--- FE/src/routes/user/MyOrder/MyOrderContainer.js
+++ FE/src/routes/user/MyOrder/MyOrderContainer.js
@@ -7,55 +7,8 @@ import Stomp from 'stompjs';
 
 const MyOrderContainer = () => {
   const { me } = useSelector(state => state.user);
-  const [orders, setOrders] = useState(null);
+  const [orders, setOrders] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
-  const localToken = localStorage.getItem('TOKEN');
-  const sessionToken = sessionStorage.getItem('TOKEN');
-  const token = localToken
-    ? `Bearer ${localToken}`
-    : '' || sessionToken
-    ? `Bearer ${sessionToken}`
-    : '';
-
-  // const [currentOrderList, setCurrentOrderList] = useState([]);
-  const client = Stomp.over(
-    new SockJS('/teamonion', null, {
-      headers: { Authorization: token },
-    }),
-  );
-
-  const socketMyOrderInit = () => {
-    client.connect({}, async frame => {
-      console.log(frame);
-      alert(`socket conneted: ${frame}`);
-      await client.subscribe('/topic/order', msg => {
-        console.log('message : ' + msg);
-        const newArrayOrders = orders;
-        const changedData = msg.body && JSON.parse(msg.body);
-        console.log('changedData:' + changedData);
-        console.log('orders:' + orders);
-        const changedDataIndex =
-          orders &&
-          orders.length > 0 &&
-          orders.findIndex(e => {
-            return e.id === changedData.id;
-          });
-        if (newArrayOrders && newArrayOrders.length > 0) {
-          newArrayOrders[changedDataIndex] = {
-            ...orders[changedDataIndex],
-            made: changedData.made,
-            paid: changedData.paid,
-          };
-          if (changedData.pickup === true) {
-            newArrayOrders = newArrayOrders.slice(changedDataIndex);
-          }
-        }
-        setOrders(...newArrayOrders);
-      });
-      // client.send('/api/orders/update', {}, JSON.stringify({ memberId: me.id }));
-    });
-  };
 
   useEffect(() => {
     const fetchMyOrder = async () => {
@@ -65,25 +18,15 @@ const MyOrderContainer = () => {
             data: { content },
           } = await userOrderAPI(me.id, false);
           // console.log(content);
-          await setOrders(content);
+          await setOrders([...content]);
         }
       } catch (e) {
         console.log(e);
       }
-      await socketMyOrderInit();
     };
     fetchMyOrder();
     setIsLoading(false);
     // console.log(orders);
-    return () => {
-      try {
-        client.disconnect(() => {
-          alert('socket disconnected!');
-        });
-      } catch (e) {
-        console.log(e);
-      }
-    };
   }, []);
 
   return (
--- FE/src/routes/user/MyOrder/MyOrderPresenter.js
+++ FE/src/routes/user/MyOrder/MyOrderPresenter.js
@@ -1,38 +1,103 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import propTypes from 'prop-types';
 import MyOrderCard from './components/MyOrderCard';
 import './styles/MyOrderPresenter.scss';
 import Loading from '../../../components/Loading';
 
-const MyOrderPresenter = ({ isLoading, orders, setOrders, userId }) => (
-  <>
-    {isLoading && <Loading />}
-    <div className="myorder-wrapper">
-      {orders &&
-        orders.length > 0 &&
-        orders.map(
-          (order, index) =>
-            !order.pickup && (
-              <MyOrderCard
-                key={index}
-                made={order.made}
-                paid={order.paid}
-                menu={order.menuNameList}
-                setOrders={setOrders}
-                userId={userId}
-              />
-            ),
+const MyOrderPresenter = ({ isLoading, orders, setOrders, userId }) => {
+  const localToken = localStorage.getItem('TOKEN');
+  const sessionToken = sessionStorage.getItem('TOKEN');
+  const token = localToken
+    ? `Bearer ${localToken}`
+    : '' || sessionToken
+    ? `Bearer ${sessionToken}`
+    : '';
+
+  // const [currentOrderList, setCurrentOrderList] = useState([]);
+  const client = Stomp.over(
+    new SockJS('/teamonion', null, {
+      headers: { Authorization: token },
+    }),
+  );
+
+  const socketMyOrderInit = () => {
+    client.connect({}, async frame => {
+      console.log(frame);
+      alert(`socket conneted: ${frame}`);
+      await client.subscribe('/topic/order', msg => {
+        console.log('message : ' + msg);
+        const newArrayOrders = [...orders];
+        const changedData = msg.body && JSON.parse(msg.body);
+        console.log('newArrayOrders' + newArrayOrders);
+        console.log('changedData:' + changedData);
+        console.log('orders:' + orders);
+
+        if (newArrayOrders.length > 0) {
+          const changedDataIndex = newArrayOrders.findIndex(e => {
+            return e.id === changedData.id;
+          });
+
+          if (changedData.pickup === true) {
+            // pick up 된거면 제거
+            newArrayOrders = newArrayOrders.slice(changedDataIndex);
+          } else {
+            newArrayOrders[changedDataIndex] = {
+              ...newArrayOrders[changedDataIndex],
+              made: changedData.made,
+              paid: changedData.paid,
+            };
+          }
+        }
+
+        setOrders([...newArrayOrders]);
+      });
+      // client.send('/api/orders/update', {}, JSON.stringify({ memberId: me.id }));
+    });
+  };
+
+  useEffect(() => {
+    socketMyOrderInit();
+
+    return () => {
+      try {
+        client.disconnect(() => {
+          alert('socket disconnected!');
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      {isLoading && <Loading />}
+      <div className="myorder-wrapper">
+        {orders.length > 0 &&
+          orders.map(
+            (order, index) =>
+              !order.pickup && (
+                <MyOrderCard
+                  key={index}
+                  made={order.made}
+                  paid={order.paid}
+                  menu={order.menuNameList}
+                  setOrders={setOrders}
+                  userId={userId}
+                />
+              ),
+          )}
+        {!isLoading && orders.length === 0 && (
+          <div className="myorder-nothing">
+            <div className="myorder-nothing-cry" />
+            <div className="myorder-nothing-empty">Empty</div>
+            <div className="myorder-nothing-uu">주문이 없어요 ㅠㅠ</div>
+          </div>
         )}
-      {!isLoading && orders && orders.length === 0 && (
-        <div className="myorder-nothing">
-          <div className="myorder-nothing-cry" />
-          <div className="myorder-nothing-empty">Empty</div>
-          <div className="myorder-nothing-uu">주문이 없어요 ㅠㅠ</div>
-        </div>
-      )}
-    </div>
-  </>
-);
+      </div>
+    </>
+  );
+};
 
 MyOrderPresenter.propTypes = {
   isLoading: propTypes.bool.isRequired,
