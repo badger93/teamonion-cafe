231f5ee5f62e3ae26cb0377bc4370fc27040bc2c
이오주 <badger@tmon.co.kr>
2019-08-05 12:28:22 +0900

    apply useMemo, useCallback to adminOrderCard

--- FE/src/routes/admin/AdminOrderManage/components/AdminOrderListItem.js
+++ FE/src/routes/admin/AdminOrderManage/components/AdminOrderListItem.js
@@ -1,58 +1,78 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import propTypes from 'prop-types';
-import { putOrderState } from '../../../../api/adminOrderApi'; // putOrderState(callback, 리스트, 변경할 State)
 import '../styles/AdminOrderListItem.scss';
 
-const AdminOrderListItem = ({ list, setCurrentOrderList, socketSetOrderState }) => {
+const AdminOrderListItem = ({ list, socketSetOrderState }) => {
   const { order_id, menus, made, paid, createdDate, amount, member_id } = list;
+
+  const handleChangeState = useCallback(
+    (list, state) => {
+      socketSetOrderState(list, state);
+    },
+    [socketSetOrderState],
+  );
+
+  // 메뉴 아이콘 ui
   const alignMenus = menus.map((item, index) => {
     const result = <li key={index}>{item}</li>;
     return result;
   }, '');
 
+  // 메뉴 버튼 조건렌더
+  const memoMakeBtn = useMemo(() => {
+    return made ? (
+      <></>
+    ) : (
+      <input
+        type="button"
+        value="→"
+        onClick={() => {
+          handleChangeState(list, { made: true });
+        }}
+        className="madeBtn"
+      />
+    );
+  }, [made, list, handleChangeState]);
+
+  // 결제버튼 조건렌더
+  const memoPaidBtn = useMemo(() => {
+    return paid ? (
+      <div className="paid">결제완료</div>
+    ) : (
+      <>
+        <div className="non-paid">{`결제금액: ${amount} 원`}</div>
+        <input
+          className="payBtn"
+          type="button"
+          value="결제하기"
+          onClick={() => handleChangeState(list, { paid: true })}
+        />
+      </>
+    );
+  }, [amount, paid, list, handleChangeState]);
+
+  // 픽업버튼 조건렌더
+  const memoPickupBtn = useMemo(() => {
+    return made && paid ? (
+      <input
+        className="pickupBtn"
+        type="button"
+        value="PickUp"
+        onClick={() => handleChangeState(list, { pickup: true })}
+      />
+    ) : (
+      <></>
+    );
+  }, [made, paid, list, handleChangeState]);
+
   return (
     <div className="AdminOrderListItem">
-      {made ? (
-        <></>
-      ) : (
-        <input
-          type="button"
-          value="→"
-          onClick={() => {
-            // putOrderState(setCurrentOrderList, list, { made: true });
-            socketSetOrderState(list, { made: true });
-          }}
-          className="madeBtn"
-        />
-      )}
+      {memoMakeBtn}
       <div className="orderNum">{`주문번호. ${order_id}`}</div>
       <div className="member_id">{`ID:  ${member_id}`}</div>
       <ul>{alignMenus}</ul>
-      <div className="payArea">
-        {paid ? (
-          <div className="paid">결제완료</div>
-        ) : (
-          <>
-            <div className="non-paid">{`결제금액: ${amount} 원`}</div>
-            <input
-              className="payBtn"
-              type="button"
-              value="결제하기"
-              onClick={() => socketSetOrderState(list, { paid: true })}
-            />
-          </>
-        )}
-      </div>
-      {made && paid ? (
-        <input
-          className="pickupBtn"
-          type="button"
-          value="PickUp"
-          onClick={() => socketSetOrderState(list, { pickup: true })}
-        />
-      ) : (
-        <></>
-      )}
+      <div className="payArea">{memoPaidBtn}</div>
+      {memoPickupBtn}
       <div className="createdDate">{createdDate}</div>
     </div>
   );
@@ -60,14 +80,14 @@ const AdminOrderListItem = ({ list, setCurrentOrderList, socketSetOrderState }) 
 
 AdminOrderListItem.defaultProps = {
   list: [],
-  setCurrentOrderList: () => {},
+  socketSetOrderState: () => {},
 };
 
 AdminOrderListItem.propTypes = {
   list: propTypes.objectOf(
     propTypes.oneOfType([propTypes.number, propTypes.string, propTypes.array, propTypes.bool]),
   ),
-  setCurrentOrderList: propTypes.func,
+  socketSetOrderState: propTypes.func,
 };
 
 export default AdminOrderListItem;
