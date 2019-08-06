780b4689cbe71061d8fda3d24146645e91812352
이창환 <nowmoon77@tmon.co.kr>
2019-08-06 12:27:16 +0900

    Refactor : pageable default - sort by id, desc

--- BE/tmong/src/main/java/com/teamonion/tmong/member/MemberController.java
+++ BE/tmong/src/main/java/com/teamonion/tmong/member/MemberController.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -39,7 +41,8 @@ public class MemberController {
 
     @CheckJwt
     @GetMapping
-    public ResponseEntity<Page<Member>> getMembers(Pageable pageable) {
+    public ResponseEntity<Page<Member>> getMembers(
+            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
         return new ResponseEntity<>(memberService.getMembers(pageable), HttpStatus.OK);
     }
 
--- BE/tmong/src/main/java/com/teamonion/tmong/order/OrdersController.java
+++ BE/tmong/src/main/java/com/teamonion/tmong/order/OrdersController.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -43,7 +45,8 @@ public class OrdersController {
 
     @CheckJwt
     @GetMapping("/my")
-    public ResponseEntity<Page<OrdersResponse>> getMyOrders(Pageable pageable, boolean pickup) {
+    public ResponseEntity<Page<OrdersResponse>> getMyOrders(
+            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable, boolean pickup) {
         return new ResponseEntity<>(ordersService.getMyOrders(pageable, pickup), HttpStatus.OK);
     }
 
@@ -65,7 +68,8 @@ public class OrdersController {
 
     @CheckJwt
     @GetMapping
-    public ResponseEntity<Page<OrdersResponse>> getOrdersByCategory(Pageable pageable, String category) {
+    public ResponseEntity<Page<OrdersResponse>> getOrdersByCategory(
+            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable, String category) {
         return new ResponseEntity<>(ordersService.getOrdersByCategory(pageable, category), HttpStatus.OK);
     }
 
