8cd4a4fca3f8ebc4f4aa2e678c78649c559ac6db
이창환 <nowmoon77@tmon.co.kr>
2019-08-08 17:07:50 +0900

    Fix : native query - from statistics lowcase

--- BE/tmong/src/main/java/com/teamonion/tmong/statistics/StatisticsRepository.java
+++ BE/tmong/src/main/java/com/teamonion/tmong/statistics/StatisticsRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 public interface StatisticsRepository extends JpaRepository<Statistics, Long> {
     Optional<Statistics> findByMemberId(String memberId);
 
-    @Query(value = "select * from Statistics where month=?1 order by orders_count desc, modified_date limit 3", nativeQuery = true)
+    @Query(value = "select * from statistics where month=?1 order by orders_count desc, modified_date limit 3", nativeQuery = true)
     List<Statistics> findTop3ByMonth(int month);
 }
