20bddb202eb3a31c1b1b2dc1a29063c540224c38
이오주 <badger@tmon.co.kr>
2019-08-09 11:56:24 +0900

    usememo, cleartimeout 적용

--- FE/src/routes/admin/AdminOrderManage/AdminOrderManagePresenter.js
+++ FE/src/routes/admin/AdminOrderManage/AdminOrderManagePresenter.js
@@ -1,15 +1,21 @@
 /* eslint-disable no-nested-ternary */
-import React from 'react';
+import React, { useMemo } from 'react';
 import propTypes from 'prop-types';
 import AdminMakingArea from './components/AdminMakingArea';
 import './styles/AdminOrderManagePresenter.scss';
 
 const AdminOrderManagePresenter = ({ currentOrderList, socketSetOrderState }) => {
-  const beforeList = currentOrderList.filter(item => item.made === false && item.pickup === false);
-  const afterList = currentOrderList.filter(item => item.made === true && item.pickup === false);
-  // 주문 리스트정렬 제작중엔 미결제가 상단, 제작 완료시 결제완료된 것이 상단, 같은 조건시에는 주문번호순 정렬
-  beforeList.sort((a, b) => (a.paid < b.paid ? -1 : a.paid > b.paid ? 1 : 0));
-  afterList.sort((a, b) => (a.paid > b.paid ? -1 : a.paid < b.paid ? 1 : 0));
+  const beforeList = useMemo(() => {
+    const res = currentOrderList.filter(item => item.made === false && item.pickup === false);
+    // 주문 리스트정렬 제작중엔 미결제가 상단, 제작 완료시 결제완료된 것이 상단, 같은 조건시에는 주문번호순 정렬
+    res.sort((a, b) => (a.paid < b.paid ? -1 : a.paid > b.paid ? 1 : 0));
+    return res;
+  }, [currentOrderList]);
+  const afterList = useMemo(() => {
+    const res = currentOrderList.filter(item => item.made === true && item.pickup === false);
+    res.sort((a, b) => (a.paid > b.paid ? -1 : a.paid < b.paid ? 1 : 0));
+    return res;
+  }, [currentOrderList]);
 
   return (
     <div className="AdminOrderManagePresenter">
