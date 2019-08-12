87302e60637c6f50edb2aca4589b7842938bcece
김다희 <sefide36@tmon.co.kr>
2019-08-12 10:15:40 +0900

    Refactor Menu API

--- BE/tmong/src/main/java/com/teamonion/tmong/menu/MenuRepository.java
+++ BE/tmong/src/main/java/com/teamonion/tmong/menu/MenuRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
@@ -12,6 +13,9 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
 
     Page<Menu> findAllByDeletedFalse(Pageable pageable);
 
+    List<Menu> findByDeletedFalseAndIdIn(List<Long> menuIds);
+
+    // TODO : 커넥션 줄이기
     Optional<Menu> findByIdAndDeletedFalse(Long id);
 
 }
