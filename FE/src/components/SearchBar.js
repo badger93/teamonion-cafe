6c5e4e9201f653497853a1ab97304fcab7da31f3
이오주 <badger@tmon.co.kr>
2019-08-13 15:53:13 +0900

    검색어 특수문자 막기 좌우공백 trim

--- FE/src/components/SearchBar.js
+++ FE/src/components/SearchBar.js
@@ -8,11 +8,28 @@ const SearchBar = ({ searchCallback }) => {
   const [searchText, setSearchText] = useState('');
   const [isSearchOpen, setIsSearchOpen] = useState(false);
 
+  // 특수문자있는지 체크
+  const isSpecial = str => {
+    const reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-+<>@\#$%&\\\=\(\'\"]/gi;
+    return reg.test(str);
+  };
+
+  // 문자 좌우 공백 제거
+  const trimStr = str => {
+    return str.replace(/^\s+|\s+$/g, '');
+  };
+
   const submitCallback = useCallback(
     e => {
       e.preventDefault();
-      searchCallback(searchText);
-      setSearchText('');
+      if (!isSpecial) {
+        alert('검색어에 특수문자를 포함 할 수 없습니다');
+        setSearchText('');
+      } else {
+        const trimedStr = trimStr(searchText);
+        searchCallback(trimedStr);
+        setSearchText('');
+      }
     },
     [searchText, searchCallback],
   );
