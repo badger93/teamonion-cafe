1f258bbe09067055ae737a88ed525028f5ee3f58
hyunjae <steveysung91@gmail.com>
2019-08-14 10:28:20 +0900

    useShowupString 메모리 누수 처리2

--- FE/src/utils/signUpForm.js
+++ FE/src/utils/signUpForm.js
@@ -1,8 +1,6 @@
 import { useState, useCallback, useRef } from 'react';
 
 export const useShowupString = string => {
-  let timeOut = null;
-  const timeOutRef = useRef();
   const [showupString, setShowupString] = useState('');
   const [isShowing, setIsShowing] = useState(false);
   const setShowupStringFunc = useCallback(
@@ -11,12 +9,12 @@ export const useShowupString = string => {
       setIsShowing(true);
 
       if (isShowing) {
-        timeOutRef.current = setTimeout(() => {
+        setTimeout(() => {
           setIsShowing(false);
         }, 2000);
       }
     },
-    [setShowupString, setIsShowing, timeOutRef],
+    [setShowupString, setIsShowing],
   );
 
   return { setShowupStringFunc, showupString, isShowing, setIsShowing };
