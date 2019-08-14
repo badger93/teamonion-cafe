9a41439f19f8d6226f15f8e49e67acfb54f0bbe4
hyunjae <steveysung91@gmail.com>
2019-08-14 11:42:16 +0900

    update

--- FE/src/utils/signUpForm.js
+++ FE/src/utils/signUpForm.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useRef } from 'react';
+import { useState, useCallback } from 'react';
 
 export const useShowupString = string => {
   const [showupString, setShowupString] = useState('');
